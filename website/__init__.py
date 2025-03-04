377c8f300bb652c8ba5f3d70a11c6508b7daa224
nhatvo1502 <nhatvo31415@gmail.com>
2025-03-04 15:26:13 -0800

    sign up and login

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,10 @@ def create_app():
 	app = Flask(__name__)
 	app.config['SECRET_KEY'] = 'awkgebakwbgealawbglwa' #app secret do not share
 
+	from .views import views # import all routes from views
+	from .auth import auth # import all auths from auth
+
+	app.register_blueprint(views, url_prefix='/')
+	app.register_blueprint(auth, url_prefix='/')
+
 	return app
