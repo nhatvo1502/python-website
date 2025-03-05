848de58903fbead86526b0d5902c57ea3f61deb7
nhatvo1502 <nhatvo31415@gmail.com>
2025-03-05 10:26:20 -0800

    flask login module - working

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 import os
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -9,7 +10,6 @@ DB_NAME = "database.db"
 def create_app():
 	app = Flask(__name__)
 	app.config['SECRET_KEY'] = 'awkgebakwbgealawbglwa' #app secret do not share
-   
 	app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 	db.init_app(app)
 
@@ -21,6 +21,14 @@ def create_app():
 
 	from .models import User, Note
 	create_database(app)
+
+	login_manager = LoginManager()
+	login_manager.login_view = 'auth.login'
+	login_manager.init_app(app)
+
+	@login_manager.user_loader
+	def load_user(id):
+		return User.query.get(int(id))
 
 	return app
 
