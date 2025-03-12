3cb0bac96619ae7b03b5dd79d80a69f0020c3e56
nhatvo1502 <nhatvo31415@gmail.com>
2025-03-12 13:37:11 -0700

    allow delete ecr repo when not empty

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,7 @@ resource "aws_ecs_cluster" "web" {
 resource "aws_ecr_repository" "web" {
   name                 = var.ecr_repository_name
   image_tag_mutability = "MUTABLE"
+  force_delete         = true # allow Github Action to decommision this repo when it's not empty
 
   image_scanning_configuration {
     scan_on_push = true
