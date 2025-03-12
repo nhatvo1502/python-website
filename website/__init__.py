a3a85a59845827b2ba243ef9394bb9995614d2fd
nhatvo1502 <nhatvo31415@gmail.com>
2025-03-12 09:13:38 -0700

    a

--- website/__init__.py
+++ website/__init__.py
@@ -6,10 +6,11 @@ from flask_login import LoginManager
 from sqlalchemy_utils import database_exists, create_database
 
 db = SQLAlchemy()
-DB_USERNAME = "root"
-DB_PASSWORD = "password"
-DB_HOST = "host.docker.internal"
+DB_USERNAME = "admin"
+DB_PASSWORD = "Welcome123#!"
+# DB_HOST = "host.docker.internal"
 # DB_HOST = "localhost"
+DB_HOST = "nnotedb.cyhfgtpbee5r.us-east-1.rds.amazonaws.com"
 DB_NAME = "nnote_database"
 
 
