8a598189bc1f8c1647beca13aca3b096b782e023
nhatvo1502 <nhatvo31415@gmail.com>
2025-03-12 10:06:41 -0700

    folder name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,7 @@
 terraform {
-	backend "s3" {
-		bucket = var.be_bucket_name
-	}
+  backend "s3" {
+    bucket = var.be_bucket_name
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
